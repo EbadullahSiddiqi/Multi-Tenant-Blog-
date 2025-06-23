e9cda92c420ddfb856ef5087c669457584c9ac9f
EbadullahSiddiqi <official.ebadullahsiddiqi@gmail.com>
2025-06-24 04:15:53 +0500

    Added docker container that runs a Postgresql database and also added Drizzle ORM to make using Postgresql easier

--- blog/app/org/[slug]/page.tsx
+++ blog/app/org/[slug]/page.tsx
@@ -1,11 +1,36 @@
-import Navbar from "@/app/components/Navbar";
-import React from "react";
+"use client";
 
-function OrganizationLandingPage() {
+import Navbar from "@/app/components/Navbar";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
+import React, { useState } from "react";
+
+function OrganizationLandingPage(): React.JSX.Element {
+  const [blogContent, setBlogContent] = useState("");
+  const [blogTitle, setBlogTitle] = useState("");
+
   return (
     <main>
       <Navbar />
       <h3>Organization Landing Page</h3>
+      <div className="p-10 flex flex-col items-center justify-center gap-4">
+        <Input
+          value={blogTitle}
+          onChange={(e) => {
+            setBlogTitle(e.target.value);
+          }}
+        />
+
+        <Textarea
+          value={blogContent}
+          onChange={(e) => {
+            setBlogContent(e.target.value);
+          }}
+        />
+
+        <Button className="">Create Blog</Button>
+      </div>
     </main>
   );
 }
